0cdf449782557d419ee43ccd27f44cf12b8f1afd
jethrinfox <jethrinfox@gmail.com>
2020-12-12 12:34:22 -0300

    project configuration in src/config/firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -8,21 +8,14 @@ import "firebase/firestore";
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDtcmclNgJi09LdzuDT5X3Cucaf4YLg5ik",
-    authDomain: "fire-chat-c4778.firebaseapp.com",
-    projectId: "fire-chat-c4778",
-    storageBucket: "fire-chat-c4778.appspot.com",
-    messagingSenderId: "238489637223",
-    appId: "1:238489637223:web:b5efa3a7b201db2a705fc2",
-    measurementId: "G-YKL88BX2Z7"
-    /* apiKey: "API_KEY",
+    apiKey: "API_KEY",
     authDomain: "PROJECT_ID.firebaseapp.com",
     databaseURL: "https://PROJECT_ID.firebaseio.com",
     projectId: "PROJECT_ID",
     storageBucket: "PROJECT_ID.appspot.com",
     messagingSenderId: "SENDER_ID",
     appId: "APP_ID",
-    measurementId: "G-MEASUREMENT_ID", */
+    measurementId: "G-MEASUREMENT_ID",
 };
 
 // Initialize Firebase
